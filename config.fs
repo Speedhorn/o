706ca19fade737542f445c491b9af9bfce849b9d
haran <harangovindan@gmail.com>
2018-10-08 10:50:36 +0200

    msm8953-common: Cleanup config.fs
    * Drop /system and /system/vendor versions of daemons all of them are
      moved to /vendor as per treble

--- config.fs
+++ config.fs
@@ -28,17 +28,11 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[vendor/bin/imsrcsd]
-mode: 0755
-user: AID_SYSTEM
-group: AID_RADIO
-caps: WAKE_ALARM
-
 [vendor/bin/ims_rtp_daemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
-caps: NET_BIND_SERVICE
+caps: WAKE_ALARM
 
 [vendor/bin/cnd]
 mode: 0755
