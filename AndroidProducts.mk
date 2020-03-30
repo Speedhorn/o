e76a65e6b927a35b9dbfe39bd7773a144f3397c7
speedhorn <manishsuper577@gmail.com>
2020-03-30 02:11:10 +0200

    rosy: posp bringup * switch to permissive * build with doge kernel * nuke lineagehw

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,8 +14,7 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_rosy.mk
+    $(LOCAL_DIR)/potato_rosy.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_rosy-userdebug \
-    lineage_rosy-eng
+    potato_rosy-eng
