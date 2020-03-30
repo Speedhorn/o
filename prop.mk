dc568ca392ee47e21bd69d58f01a31ce5d53ce28
speedhorn <manishsuper577@gmail.com>
2020-03-30 03:14:04 +0200

    rosy: update some props

--- prop.mk
+++ prop.mk
@@ -85,7 +85,8 @@ dalvik.vm.heapgrowthlimit=192m \
 dalvik.vm.heapsize=512m \
 dalvik.vm.heaptargetutilization=0.75 \
 dalvik.vm.heapminfree=2m \
-dalvik.vm.heapmaxfree=8m
+dalvik.vm.heapmaxfree=8m \
+debug.sf.enable_gl_backpressure=1
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
